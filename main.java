4e1f52f5318290caf0bb2a2fbe18763363bbac1f
ButItDoesRun <90749391+ButItDoesRun@users.noreply.github.com>
2021-10-09 22:39:22 +0200

    main.java deleted

--- main.java
+++
@@ -1,9 +0,0 @@
-//This is the file
-
-public class main {
-
-    public static void main(String[] args) {
-        System.out.print("hello world");
-        System.out.print("hello world 2.0");
-    }
-}
