b2ff16609221e06675c5814bcbc0a141c61c1a85
ButItDoesRun <90749391+ButItDoesRun@users.noreply.github.com>
2021-11-04 14:54:51 +0100

    Phaser library re-added to project directory -still not tested, but git-hub sync error-fixed
    Phaser library added to html

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ $hello = "Hello dear world!... ALSO FARTS"
 <html>
 <head>
     <title>Testsite Index</title>
+    <script type = "text/javascript" src = "phaser.js"></script>
 </head>
 <body>
 <h1><?php print_r($hello) ?></h1>
